42bac1d8b9eb18a1d78bf73e0477c0322bd85c7f
jmoostland <jeanineoostland@gmail.com>
2017-10-24 19:54:21 +0200

    eerste commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "Schapen tellen";
+echo "<br>";
+for ($x = 1; $x <= 5; $x++) {
+    echo "Schaap: $x <br>";
+} 
+
+?>
+
+<?php
+echo "<br>";
+echo "Schapen tellen";
+echo "<br>";
+echo "1 schaap";
+echo "<br>";
+for ($x = 2; $x <= 10; $x++) {
+    echo "$x schapen <br>";
+} 
+
+?>
